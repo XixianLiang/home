f8e2be4ac888ccc50bc4b1ee584cad73575e2408
Xixian Liang <51265902107@stu.ecnu.edu.cn>
2024-01-03 16:56:11 +0800

    delete record.sh

--- record.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-# python3 start.py -root_path dmf/ -choice 2 -app_path [Directory of target Apk] -json_name [Folder name of the recorded dmf] -device_serial [The Android device serial]
