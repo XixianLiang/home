e3bc9dfdc918a31d55e41cbae8efdb3de0d549c2
XixianLiang <149387586+XixianLiang@users.noreply.github.com>
2024-01-03 16:52:13 +0800

    Update record.sh

---
+++ record.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+# python3 start.py -root_path dmf/ -choice 2 -app_path [Directory of target Apk] -json_name [Folder name of the recorded dmf] -device_serial [The Android device serial]
